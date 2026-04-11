67ff789fff46d8c7866978c804eb0e86aecebf6f
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-04-11 12:49:46 +0530

    Interaction with alert, prompt, and confirm. Type conversions

--- Coding/Project0/JS/one.js
+++ Coding/Project0/JS/one.js
@@ -1,1 +1,45 @@
-alert("Hello! world")
+
+// // Ask for input
+// const nm = prompt('What is your name? ');
+// alert(`Hello, ${nm}!`);
+// alert(typeof nm)
+
+// let age = prompt('How old are you? ', 100);
+// alert(`Your are ${age} years old`);
+
+// let isPresent = confirm('Are you present?');
+// alert(`isPresent = ${isPresent}`);
+// alert(`Type of data is ${typeof isPresent}`);
+
+// let value = String(isPresent);
+// alert(`After conversion ${typeof value}`);
+
+// console.log("6" / "2");
+
+// let str = "123";
+// console.log(typeof str);
+
+// let num = Number(str);
+// console.log(typeof num);
+
+// let age = Number("Twenty")
+// console.log(age)
+
+
+// console.log(Number("       123        ")); //123
+// console.log(Number("12345z")); //NaN
+// console.log(Number(true)); //1
+// console.log(Number(false)); //0
+
+
+
+console.log(Boolean(1)); //true
+console.log(Boolean(0)); //false
+console.log(Boolean("hello")); //true
+console.log(Boolean("")) //false
+console.log(Boolean(NaN)) //false
+console.log(Boolean(null)) //false
+console.log(Boolean("0")) //true
+console.log(Boolean(" ")) //true
+
+
