04f6208efce322224de09950d22d61b929129702
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-09-07 19:36:52 +0530

    Started with Objects in JS

---
+++ Coding/Project0/JS/UserDefinedFunctions/arrowone.js
@@ -0,0 +1,18 @@
+// let sum = function(a, b){
+//     return a + b;
+// };
+
+let sum = (a, b) => {
+  let result = a+b;
+  return result;  
+} 
+
+console.log(sum(5, 7));
+
+// let double = n => n * 2;
+
+// console.log(double(5));
+
+// let sayHi = () => console.log(`Hello!`);
+
+// sayHi();
