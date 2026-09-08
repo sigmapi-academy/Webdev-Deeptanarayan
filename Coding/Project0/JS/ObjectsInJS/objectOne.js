3d460e5238041eab9037ac89832b78b91b963416
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-09-08 19:56:07 +0530

    covered property in object of JS

---
+++ Coding/Project0/JS/ObjectsInJS/objectOne.js
@@ -0,0 +1,18 @@
+let user = {
+    name: "John",
+    age: 30,
+};
+
+console.log(user);
+
+//user.likes birds = true;
+
+// user[`likes birds`] = true;
+// console.log(user);
+
+// delete user[`likes birds`]
+// console.log(user)
+
+// let key = `likes birds`;
+// user[key] = true;
+// console.log(user)
---
+++ Coding/Project0/JS/ObjectsInJS/objectTwo.js
@@ -0,0 +1,23 @@
+
+let fruit = `Apple`;
+
+let bag= {
+    [fruit + `Computers`]: 5
+};
+
+
+alert( bag.AppleComputers );
+
+
+let obj = {
+    for: 1,
+    let: 2,
+    return: 3,
+    0: `test`
+};
+
+alert(`for: ${obj.for}`);
+alert(`let: ${obj.let}`);
+alert(`return: ${obj.return}`);
+alert(`0: ${obj[0]}`);
+
