21ed751daa81e93a6ec7c463cca3cee75392c3f3
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-09-15 19:58:48 +0530

    Object as reference

---
+++ Coding/Project0/JS/ObjectsInJS/objectFive.js
@@ -0,0 +1,36 @@
+// let message = "Hello";
+// let phrase = message;
+
+
+// let user ={
+//     name: `Deeptanarayan`,
+// };
+
+// let admin = user; //copy the reference
+
+// console.log(`User-name: ${user.name}`);
+// console.log(`admin: ${admin.name}`)
+
+// admin.name = `Pete`;
+
+// console.log(`User-name: ${user.name}`)
+
+
+// let a = {};
+// // let b = a; //copy the reference
+// let b = {};
+
+// console.log( a == b); //false, Both variables reference is not  same.
+// console.log(a === b); //false
+
+
+
+
+
+const user = {
+    name: `Deeptanarayan`,
+};
+
+user.name = `Deetanarayan Sarkar`;
+
+console.log(`${user.name}`)
